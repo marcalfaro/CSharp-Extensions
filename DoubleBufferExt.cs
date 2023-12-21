b4495459d22a816690ca333c0dd443bf36a94b23
Marc <marcalfaro@users.noreply.github.com>
2023-12-21 13:41:01 +1030

    Create DoubleBufferExt.cs
    Enables doublebuffering of a control

---
+++ DoubleBufferExt.cs
@@ -0,0 +1,20 @@
+    public static class DoubleBufferExt
+    {
+
+        public static void SetDoubleBuffered(this System.Windows.Forms.Control c, bool value)
+        {
+            //Taxes: Remote Desktop Connection and painting
+            //http://blogs.msdn.com/oldnewthing/archive/2006/01/03/508694.aspx
+            if (System.Windows.Forms.SystemInformation.TerminalServerSession)
+                return;
+
+            System.Reflection.PropertyInfo aProp =
+                  typeof(System.Windows.Forms.Control).GetProperty(
+                        "DoubleBuffered",
+                        System.Reflection.BindingFlags.NonPublic |
+                        System.Reflection.BindingFlags.Instance);
+
+            aProp.SetValue(c, true, null);
+        }
+
+    } //class
